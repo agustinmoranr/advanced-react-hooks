46b4238af280db2451c7da06350709e83e2875ad
Agustin Moran <agustin.moranr@gmail.com>
2021-10-04 20:16:49 -0500

    Excercise 1: useReducer Hook: simple counter

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,21 +3,93 @@
 
 import * as React from 'react'
 
-function Counter({initialCount = 0, step = 1}) {
-  // 🐨 replace React.useState with React.useReducer.
-  // 💰 React.useReducer(countReducer, initialCount)
-  const [count, setCount] = React.useState(initialCount)
+const REDUCER_TYPES = {
+  INCREMENT: 'INCREMENT',
+  DECREMENT: 'DECREMENT',
+}
 
-  // 💰 you can write the countReducer function so you don't have to make any
-  // changes to the next two lines of code! Remember:
-  // The 1st argument is called "state" - the current value of count
-  // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setCount(count + step)
-  return <button onClick={increment}>{count}</button>
+function reducerCount(state, action) {
+  // console.log({state, action})
+  //add suport to accept a function to our dispatcher
+  if (typeof action === 'function') {
+    action = action(state)
+  }
+
+  const {type, payload} = action
+
+  const currentCount = state.count
+  switch (type) {
+    case REDUCER_TYPES.INCREMENT:
+      return {count: currentCount + payload}
+
+    case REDUCER_TYPES.DECREMENT:
+      return {count: currentCount - payload}
+    default:
+      throw new Error(`Unsupported action type: ${type}`)
+  }
+}
+
+function Counter({initialCount = 0, step = 1}) {
+  const [state, stateDispatcher] = React.useReducer(reducerCount, {
+    count: initialCount,
+  })
+  const {count} = state
+
+  const increment = () =>
+    stateDispatcher({type: REDUCER_TYPES.INCREMENT, payload: step})
+
+  const decrement = () =>
+    stateDispatcher({type: REDUCER_TYPES.DECREMENT, payload: step})
+
+  return (
+    <div style={containerStyles}>
+      <p>useReducer Counter</p>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <button onClick={increment} style={buttonStyles}>
+          +
+        </button>
+        <p style={childrenStyles}>{count}</p>
+        <button onClick={decrement} style={buttonStyles}>
+          -
+        </button>
+      </div>
+    </div>
+  )
 }
 
 function App() {
   return <Counter />
+}
+
+const containerStyles = {
+  width: '100vw',
+  height: '100vh',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  fontSize: '1.5rem',
+  flexDirection: 'column',
+}
+
+const childrenStyles = {
+  margin: '12px',
+}
+
+const buttonStyles = {
+  ...childrenStyles,
+  height: '48px',
+  width: '48px',
+  borderRadius: '50%',
+  border: 'none',
+  boxShadow: '0px 0px 1px #032248f7',
+  backgroundColor: '#004cabe3',
+  color: '#ffffff',
 }
 
 export default App
